a1c37a6f408d83ce73fb564de1929a17c5718822
igor.drobiazko <igor.drobiazko@8f193268-6a68-11de-bdd1-8505e4066020>
2009-07-09 20:19:53 +0000

    Added hello-world app.

---
+++ trunk/hello-world/src/main/java/de/t5book/components/Layout.java
@@ -0,0 +1,7 @@
+package de.t5book.components;
+
+import org.apache.tapestry5.annotations.IncludeStylesheet;
+
+@IncludeStylesheet("context:style.css")
+public class Layout {
+}
---
+++ trunk/hello-world/src/main/java/de/t5book/pages/Index.java
@@ -0,0 +1,8 @@
+package de.t5book.pages;
+
+public class Index {
+
+	public String getHello() {
+		return "Hello, World!";
+	}
+}
