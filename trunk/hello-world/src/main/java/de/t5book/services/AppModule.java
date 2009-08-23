275dacc07a59ede61e20388c9396bd61edf9875e
igor.drobiazko <igor.drobiazko@8f193268-6a68-11de-bdd1-8505e4066020>
2009-08-23 11:27:33 +0000

    Fixed translation for russian locale, replaced older tap version form namespaces

--- trunk/hello-world/src/main/java/de/t5book/services/AppModule.java
+++ trunk/hello-world/src/main/java/de/t5book/services/AppModule.java
@@ -13,19 +13,10 @@ import org.apache.tapestry5.services.RequestHandler;
 import org.apache.tapestry5.services.Response;
 import org.slf4j.Logger;
 
-/**
- * This module is automatically included as part of the Tapestry IoC Registry,
- * it's a good place to configure and extend Tapestry, or to place your own
- * service definitions.
- */
 public class AppModule {
 	public static void bind(ServiceBinder binder) {
 		// binder.bind(MyServiceInterface.class, MyServiceImpl.class);
 
-		// Make bind() calls on the binder object to define most IoC services.
-		// Use service builder methods (example below) when the implementation
-		// is provided inline, or requires more initialization than simply
-		// invoking the constructor.
 	}
 
 	public static void contributeApplicationDefaults(
